15c68e78dd0c558e94ca5aac7ae7af67eaf91f11
june-yamamoto <attsu.gx@gmail.com>
2021-09-12 17:28:49 +0900

    feat redux-thunk周りの実装

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { CookieBody } from './components/Cookie/CookieBody';
 import { ItemStore } from './components/ItemStore/ItemStore';
 import { useAddNenesanInterval } from './hooks/useAddNenesanInterval';
-import useInterval from './hooks/useInterval';
 import useMount from './hooks/useMount';
-import { ClickerRootState } from './store/state';
+import { ClickerRootState, RootState } from './store/state';
 import { useEffect } from 'react';
-import { Drawer } from '@material-ui/core';
+import { Drawer, Snackbar } from '@material-ui/core';
 import { AppHeader } from './components/Header/AppHeader';
-import firebase, { db } from './firebase';
-import { collection } from 'firebase/firestore';
 import { useSecondInterval } from './hooks/useSecondInterval';
+import { useSaveToServer } from './hooks/useSaveToServer';
+import useInterval from './hooks/useInterval';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = createUseStyles({
     root: {
@@ -54,13 +54,29 @@ export const App = () => {
         return () => window.removeEventListener('resize', resizeListener);
     });
 
+    // const [isAppInit, setIsAppInit] = useState(false);
+
     useMount(() => {
         dispatch({ type: 'LOAD' });
     });
 
+    const [savedSnackbarOpen, setSavedSnackbarOpen] = useState(false);
+
     useInterval(() => {
         dispatch({ type: 'SAVE' });
-    }, 10000);
+        setSavedSnackbarOpen(true);
+    }, 60000);
+
+    // useEffect(() => {
+    //     if (currentState.name === '') {
+
+    //     } else {
+    //         setIsAppInit(true);
+    //     }
+    // }, []);
+
+    const currentState = useSelector((state: RootState) => state);
+    useSaveToServer(true, currentState, dispatch);
 
     const handleClickMenuIcon = useCallback(() => {
         setOpen(!open);
@@ -87,6 +103,18 @@ export const App = () => {
                     }}
                 >
                     <CookieBody />
+                    <Snackbar
+                        open={savedSnackbarOpen}
+                        autoHideDuration={3000}
+                        onClose={() => setSavedSnackbarOpen(false)}
+                        anchorOrigin={
+                            windowWidth <= 540
+                                ? { vertical: 'bottom', horizontal: 'center' }
+                                : { vertical: 'bottom', horizontal: 'left' }
+                        }
+                    >
+                        <Alert>セーブしました</Alert>
+                    </Snackbar>
                 </div>
                 {windowWidth <= 540 ? (
                     <Drawer
