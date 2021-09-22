697f9d4098f12da82434b891b8f47dbde64cebe0
june-yamamoto <attsu.gx@gmail.com>
2021-09-23 02:30:24 +0900

    feat DIALOGUEメンバー固有値を扱う配列を実装

---
+++ src/static/resource/dialogue_array.ts
@@ -0,0 +1,19 @@
+import yurinya from '../image/yurinya_icon.png';
+import nenesan from '../image/nenesan_icon.png';
+import kyon from '../image/kyon_icon.png';
+import yuna from '../image/yuna_icon.png';
+import yakan from '../image/yakan_icon.png';
+import sappi from '../image/sappi_icon.png';
+import mayu from '../image/mayu_icon.png';
+import manat from '../image/manat_icon.png';
+
+export const DIALOGUE_ARRAY = [
+  {name: 'yurinya', color: '#f39aab', supportProbability: 13, imgSrc: yurinya},
+  {name: 'nenesan', color: '#ffd93c', supportProbability: 9, imgSrc: nenesan},
+  {name: 'kyon', color: '#cfd10c', supportProbability: 13, imgSrc: kyon},
+  {name: 'yuna', color: '#15addc', supportProbability: 13, imgSrc: yuna},
+  {name: 'yakan', color: '#ff9700', supportProbability: 13, imgSrc: yakan},
+  {name: 'sappi', color: '#f15183', supportProbability: 13, imgSrc: sappi},
+  {name: 'mayu', color: '#80c4ed', supportProbability: 13, imgSrc: mayu},
+  {name: 'manat', color: '#a269a8', supportProbability: 13, imgSrc: manat},
+]
